581fdd790b459a66ebe8e3762ba94246ad2d8b3e
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-09-22 18:59:18 +0530

    Add files via upload

---
+++ loop3.c
@@ -0,0 +1,21 @@
+/* c program to accept an integer and check if it is prime or not */
+#include<stdio.h>
+void  main()
+{
+	int n,f=0,i=2;
+	printf("enter any number=");
+	scanf("%d",&n);
+	while(i<n)
+	{
+		if(n%i==0)
+		{
+			f=1;
+			break;
+		}
+		i++;
+	}
+	if(f==0)
+	printf("given integer is prime.");
+	else
+	printf("given integer is not prime.");
+}

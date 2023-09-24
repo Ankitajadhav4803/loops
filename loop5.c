a7063886bd013d93f94cd0b378fec235e909ff37
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-09-24 17:10:51 +0530

    Add files via upload

---
+++ loop5.c
@@ -0,0 +1,15 @@
+/* c program to accept an integer and reverse the number */
+#include<stdio.h>
+void  main()
+{
+	int n,d,rev=0;
+	printf("enter any number=");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		d=n%10;
+		rev=(rev*10)+d;
+		n=n/10;
+	}
+	printf("\n reverse number of the given number=%d",rev);
+}
